1849fbda99622425ab92e4c4545d4f7df2ded6c2
Rutuja Chaudhari <58293326+rutuja191919@users.noreply.github.com>
2022-07-05 19:04:07 +0530

    Add files via upload

---
+++ srcParenttochild/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent 
+{
+  public Message : string = "Hello Child Component ! This is message from parent"; 
+}
